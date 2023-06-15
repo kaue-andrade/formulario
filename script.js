2c74e8ea6508dc28da1009e1b989abdd2e497a8d
Kauê Andrade <91803383+kaue-andrade@users.noreply.github.com>
2023-06-15 17:43:37 -0300

    form

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
 function mostrarMensagem() {
-
-    var nome = document.getElementById('nome').value;
-
-    var mensagem = "Seja bem-vindo, " + nome + "!"
     
-    alert(mensagem);
+    var nome = document.getElementById('nome').value;
+    var email = document.getElementById('email').value;
+    var cpf = document.getElementById('cpf').value;
 
+    if (nome.length > 30){
+        alert("Nome aceitável!");
+
+        if (email.indexOf('@') !== -1 && email.indexOf('@') > 0 && (email.endsWith('.com') || email.endsWith('.edu'))){
+
+            alert("E-mail válido!");
+
+            var cpfRegex = /^\d{3}\.\d{3}.\d{3}-\d{2}$/;
+
+            if (cpfRegex.test(cpf)){
+                alert("CPF aceitável!");
+
+                var mensagem = "Seja bem-vindo, " + nome + "!"
+    
+                alert(mensagem);
+
+            } else{
+                alert("CPF não aceito!");
+                return;
+    }
+        } else {
+            alert("E-mail não aceito!")
+            return;
+        }
+    } else {
+        alert("Nome não aceito!");
+        return;
+    }
 }
